c0ebe0bcb3b7b77826692f48f677f0669405d2f6
Guillaume Caugant <caugant@et.esiea.fr>
2020-03-28 13:48:52 +0100

    Button retour fonctionnel

---
+++ app/src/main/java/com/example/pokeapi/DetailActivity.java
@@ -0,0 +1,30 @@
+package com.example.pokeapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+    private Button breturn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_country);
+        breturn = (Button) findViewById(R.id.recycler_view);
+
+        breturn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(DetailActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
